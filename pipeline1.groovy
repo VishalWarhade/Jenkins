45ff3a8cf3fd451b0bdec0552eaa3019402b0c0c
vishal <vishal.warhade7@gmail.com>
2023-05-04 19:43:18 +0530

    fdvfd

---
+++ pipeline1.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+    stages {
+        stage ('pull') {
+            steps {
+                echo 'pull success'
+            }
+        }
+        stage ('build') {
+            steps {
+                echo 'build success'
+            }
+        }
+        stage ('test') {
+            steps {
+                echo 'test success'
+            }
+        }
+        stage ('deploy') {
+            steps {
+                echo 'deploy success'
+            }
+        }
+
+    }
+}
