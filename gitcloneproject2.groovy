22ae34663ebbc7d2a64c2a092cd9823d6ec00427
vishal <vishal.warhade7@gmail.com>
2023-05-05 19:03:10 +0530

    vvdx

---
+++ gitcloneproject2.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage ('pull') {
+            steps {
+                git 'https://github.com/VishalWarhade/Jenkins.git'
+            }
+        }
+        stage ('build') {
+            steps {
+                echo 'build success'
+            }
+        }
+        stage ('test') {
+            steps {
+                echo 'test success'
+            }
+        }
+        stage ('deploy') {
+            steps {
+                echo 'deploy success'
+            }
+        }
+    }
+}
