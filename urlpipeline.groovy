2f53fce1c23e74c36aa3bd8f02f8506b09d923f8
vishal <vishal.warhade7@gmail.com>
2023-05-05 17:15:32 +0530

    feerfw

--- urlpipeline.groovy
+++ urlpipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('pull') {
             steps {
-                echo 'https://github.com/VishalWarhade/Jenkins.git'
+                git 'https://github.com/VishalWarhade/Jenkins.git'
             }
         }
         stage ('build') {
