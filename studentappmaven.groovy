1542e522018d1c29056bb9b6251f3abcdc6ea9e1
vishal <vishal.warhade7@gmail.com>
2023-05-06 13:27:25 +0530

    hjk

--- studentappmaven.groovy
+++ studentappmaven.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('pull') {
             steps {
-                git branch: 'main', url: 'https://github.com/VishalWarhade/Jenkins.git'
+                git branch: 'main', url: 'https://github.com/VishalWarhade/Jenkins/studentapp-ui-master.git'
             }
         }
         stage ('build') {
