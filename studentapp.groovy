efd438f2eb09357ac74febd1036df2d97ee6d0c9
vishal <vishal.warhade7@gmail.com>
2023-05-09 17:09:48 +0530

    dfghj

--- studentapp.groovy
+++ studentapp.groovy
@@ -13,7 +13,7 @@ pipeline {
         }
         stage ('test') {
             steps {
-                sh '/opt/apache-maven/bin/mvn sonar:sonar  -Dsonar.projectKey=studentapp  -Dsonar.host.url=http://43.207.194.247:9000  -Dsonar.login=sonar-token'
+                sh '/opt/apache-maven/bin/mvn sonar:sonar  -Dsonar.projectKey=studentapp  -Dsonar.host.url=http://43.207.194.247:9000  -Dsonar.login=7d1f21bd14af328742a92f93536c616ae8bc5b4c'
             }
         }
         stage ('deploy') {
